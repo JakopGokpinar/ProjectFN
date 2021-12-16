7dce9b32ff1e98b8eb4ec5a9135904861ccf731e
AhmetGkp <ahmettabar2003@gmail.com>
2021-12-16 10:22:13 -0600

    ini

--- server/controllers/FileController.js
+++ server/controllers/FileController.js
@@ -112,6 +112,8 @@ createAnnonce = async (req,res, next) => {
     .then(res.json({message: "new annonce created", locations: imgLocations}))
     .catch(err => res.json({error: err}));
 
+    
+
     annoncesDb.collection("annonces").insertOne({
       _id: newAnnonce.id,
       annonce: newAnnonce
